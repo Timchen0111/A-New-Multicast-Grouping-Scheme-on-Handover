59635f8ec7cb4e87461a3658aae53e967a4adf84
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2023-04-25 16:33:44 +0800

    try

---
+++ main.m
@@ -0,0 +1,32 @@
+function main = main(mode,UE_num,time,groupsize,pptimer)
+tic
+dropnum = floor(UE_num/10);
+dropout = 10;
+K = floor(UE_num/groupsize);
+switch mode
+     case 'unicast'
+        error('Not finished')
+     case 'greedy' 
+        error('Not finished')
+     case 'kmeans'
+        disp('K-means');
+        main = simulation(UE_num,time,dropnum,dropout,K,'kmeans',pptimer);
+     case 'random'
+        disp('Grouping randomly');
+        main = simulation(UE_num,time,dropnum,dropout,K,'random',pptimer);
+     case 'GRPPD'
+        disp('Grouping randomly and Ping-Pong Detection');
+        main = simulation(UE_num,time,dropnum,dropout,K,'GRPPD',pptimer);
+    case 'GKPPD'
+        disp('Grouping with Ping-Pong Detection and Kmeans.');
+        main = simulation(UE_num,time,dropnum,dropout,K,'GKPPD',pptimer);
+     case 'GRPPD-UNI'
+        disp('Grouping randomly with Ping-Pong Detection. Using unicast for UE in HO.');
+        main = simulation_uni(UE_num,time,dropnum,dropout,K,'GRPPD',pptimer);
+     case 'GKPPD-UNI'
+        disp('Grouping with Ping-Pong Detection and Kmeans. Using unicast for UE in HO.');
+        main = simulation_uni(UE_num,time,dropnum,dropout,K,'GKPPD',pptimer);
+     otherwise
+        error( 'WRONG USE.' )
+end
+toc
