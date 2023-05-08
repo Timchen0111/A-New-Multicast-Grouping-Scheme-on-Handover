8d57a73bfbbbaadb427a5bffe900990aec7b657f
Timchen0111 <timchen910111@gmial.com>
2023-05-08 23:01:21 +0800

    Adjust unicast. Test github on the device.

--- overall.m
+++ overall.m
@@ -8,14 +8,18 @@
 % end
 % all = [uni r k gr gk gru gku];
 % writetable(all, 'result.csv');
-parfor i = 1:4
-    %a1(i,:) = main('unicast',50,3000,10,20);
-    %a2(i,:) = main('random',50,3000,10,20);
-    %a3(i,:) = main('kmeans',50,3000,10,20);
+clear all
+parfor i = 1:12
+    if i <=4
+        a1(i,:) = main('unicast',50,3000,5,10);
+    elseif i>8
+        a2(i-8,:) = main('GRPPD-UNI',50,3000,15,10);
+    else
+        a3(i-4,:) = main('GKPPD-UNI',50,3000,15,10);
+    end
     %a1(i,:) = main('GKPPD',50,3000,10,5);
     %a2(i,:) = main('GKPPD',50,3000,10,10);
     %a6(i,:) = main('GRPPD-UNI',50,3000,10,20);
-    a2(i,:) = main('GKPPD-UNI',50,3000,10,5);
 end
 
 
