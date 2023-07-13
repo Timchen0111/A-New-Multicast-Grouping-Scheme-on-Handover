de9131de648eef559821db9f2db1556be1009f96
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2023-07-13 16:18:56 +0800

    Update bw_allocation.m

--- bw_allocation.m
+++ bw_allocation.m
@@ -1,6 +1,5 @@
 function g = bw_allocation(g,bw)
 for i = 1:7
-    g(i)
     group_rate = zeros(1,max(g(i).group));
     for group = 1:max(g(i).group)
         SINR = g(i).worstSINR(group);
@@ -10,12 +9,9 @@ for i = 1:7
         SINR = 10^(SINR/10);
         group_rate(group) = log2(1+SINR);
     end
-    %disp(group_rate)
-    %disp('------------------------------------------------')
     %Calculate the ratio
     all = prod(group_rate(group_rate~=0));
     bw_ratio = zeros(1,max(g(i).group));
-    %bw_allocate = zeros(1,max(g(i).group));
     for group = 1:length(group_rate)
         if group_rate(group) == 0
             continue
