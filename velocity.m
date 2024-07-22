6c54c2d19d025ad3751e312b4a872b05ac7731b5
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2024-07-22 15:14:49 +0800

    Update velocity.m

--- velocity.m
+++ velocity.m
@@ -1,23 +1,29 @@
-function v = velocity(pos,vv)
+function v = velocity(pos,vv,gNB)
     x = pos(1);
     y = pos(2);
     angle = rand*2*pi;
     set = false;
     count = 1;
-    
+    %3km/hr = 3000m/hr = 10/12m/s = 1/12 m/(0.1s)
+    vvv = 1/1.2;
     while set == false
-        v(1) = (1/12)*cos(angle);
-        v(2) = (1/12)*sin(angle);
-        set = boundary(x+v(1),y+v(2));
+        v(1) = (vvv)*cos(angle); %Now: 60km/hr
+        v(2) = (vvv)*sin(angle);
+        set = boundary(x+v(1),y+v(2),gNB);
         count = count+1;
-        if count >1000
-            disp('overflow!')
-            if vv(1) == 0
-                error('Oops.Bad initial position.Please excute again.')
+        if count>10000
+            if numel(vv)>0
+                disp('overflow!')                
+                v(1) = -vv(1);
+                v(2) = -vv(2);
+                break  
+            else                
+                length = sqrt(x^2+y^2);
+                v(1) = -(vvv)*x/length;
+                v(2) = -(vvv)*y/length;
+                break
             end
-            v(1) = -vv(1);
-            v(2) = -vv(2);
-            break
         end
     end
+        
         
