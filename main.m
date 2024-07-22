782e9f94e004773fe13a156de1be5f83fd840e57
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2024-07-22 15:22:19 +0800

    Update main.m

--- main.m
+++ main.m
@@ -5,12 +5,10 @@ switch mode
        case 2
           main = simulation(UE_num,time,MRN,RM,'unicast',pptimer,handover,groupsize,outage);
        case 3
-          main = simulation(UE_num,time,MRN,RM,'kmeans',pptimer,handover,groupsize,outage);
-       case 4
           main = simulation(UE_num,time,MRN,RM,'ours',pptimer,handover,groupsize,outage);
-       case 5
+       case 4
           main = simulation(UE_num,time,MRN,RM,'VG',pptimer,handover,groupsize,outage);
-       case 6
+       case 5
           main = simulation(UE_num,time,MRN,RM,'broadcast',pptimer,handover,groupsize,outage)
        otherwise
          error('WRONG USE.') 
